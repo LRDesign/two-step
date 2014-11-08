e9689cbe0af0c0902b892763e47b63d1cb721b6d
Judson <nyarly@gmail.com>
2014-11-07 16:14:05 -0800

    Attempting to recover RSpec2 compat

--- lib/rspec-steps/duckpunch/object-extensions.rb
+++ lib/rspec-steps/duckpunch/object-extensions.rb
@@ -3,8 +3,6 @@ require 'rspec-steps/stepwise'
 require 'rspec/core/shared_example_group'
 
 module RSpec::Core::SharedExampleGroup
-  alias shared_steps shared_examples_for
-  if respond_to? :share_as
-    alias steps_shared_as share_as
-  end
+  alias :shared_steps :share_examples_for
+  alias :steps_shared_as :share_as
 end
